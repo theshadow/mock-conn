0f48c8527e519c34869ba3fdf70fafa51e8a4d2d
Xander Guzman <xguzman@efolder.net>
2016-02-18 09:32:45 -0700

    Made mock_conn.Conn actually implement net.Conn

--- mock_conn.go
+++ mock_conn.go
@@ -1,6 +1,14 @@
 package mock_conn
 
-import "io"
+import (
+	"io"
+	"net"
+	"time"
+)
+
+type Addr struct {
+	net.TCPAddr
+}
 
 // MockConn facilitates testing by providing two connected ReadWriteClosers
 // each of which can be used in place of a net.Conn
@@ -37,5 +45,47 @@ func (c *Conn) Close() error {
 	if err := c.Client.Close(); err != nil {
 		return err
 	}
+	return nil
+}
+
+
+// LocalAddr returns the local network address.
+func (c *Conn) LocalAddr() Addr {
+	return Addr{}
+}
+
+// RemoteAddr returns the remote network address.
+func (c *Conn) RemoteAddr() Addr {
+	return Addr{}
+}
+
+// SetDeadline sets the read and write deadlines associated
+// with the connection. It is equivalent to calling both
+// SetReadDeadline and SetWriteDeadline.
+//
+// A deadline is an absolute time after which I/O operations
+// fail with a timeout (see type Error) instead of
+// blocking. The deadline applies to all future I/O, not just
+// the immediately following call to Read or Write.
+//
+// An idle timeout can be implemented by repeatedly extending
+// the deadline after successful Read or Write calls.
+//
+// A zero value for t means I/O operations will not time out.
+func (c *Conn) SetDeadline(t time.Time) error {
+	return nil
+}
+
+// SetReadDeadline sets the deadline for future Read calls.
+// A zero value for t means Read will not time out.
+func (c *Conn) SetReadDeadline(t time.Time) error {
+	return nil
+}
+
+// SetWriteDeadline sets the deadline for future Write calls.
+// Even if write times out, it may return n > 0, indicating that
+// some of the data was successfully written.
+// A zero value for t means Write will not time out.
+func (c *Conn) SetWriteDeadline(t time.Time) error {
 	return nil
 }
